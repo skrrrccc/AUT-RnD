2c7410434dd25c19202fea8ade29e1b8d9b20c00
skrrrccc <54016154+skrrrccc@users.noreply.github.com>
2021-03-17 19:01:09 +1300

    Add files via upload

---
+++ src/pages/InnerEar/Mice.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
+import { Card, Alert, Typography } from 'antd';
+import { useIntl, FormattedMessage } from 'umi';
+import styles from './Welcome.less';
+
+export default (): React.ReactNode => {
+    const intl = useIntl();
+    return (
+        <PageContainer>
+            <Card>
+                <Alert
+                    message="comming soon"
+                    type="warning"
+                    showIcon
+                    banner
+                    style={{
+                        margin: -12,
+                        marginBottom: 24,
+                    }}
+                />
+            </Card>
+        </PageContainer>
+    );
+};
