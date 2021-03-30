50d0931db069aafdda5ff19371c3779835f46975
skrrrccc <54016154+skrrrccc@users.noreply.github.com>
2021-03-30 17:38:35 +0800

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -11,6 +11,18 @@ export default () => (
         title: 'AUT', 
         href: 'https://www.aut.ac.nz/study/study-options/engineering-computer-and-mathematical-sciences/ecms-poster-showcase/bachelor-of-computer-and-information-sciences',
         blankTarget: true
+      },
+      {
+        key: 'UOA', 
+        title: 'UOA', 
+        href: 'https://www.fmhs.auckland.ac.nz/en/sms/about/our-departments/physiology/research-groups/auditory-neurobiology.html?fbclid=IwAR2FC-77hVeRTNDayDZ1Y_B2KtQagUXHfXUQaJgc7Fyi7gjUh2ng49SJEMU',
+        blankTarget: true
+      },
+      {
+        key: 'Facebook', 
+        title: 'Facebook', 
+        href: 'https://www.facebook.com/AVTNC1/',
+        blankTarget: true
       }
     ]}
   />
