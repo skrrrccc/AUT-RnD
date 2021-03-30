6a5d5df2565e78de35372af3c3f3264ef2de3b4e
skrrrccc <54016154+skrrrccc@users.noreply.github.com>
2021-03-30 18:49:59 +0800

    Update ContactUs.tsx

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Card, Alert, Typography } from 'antd';
+import { Form, Input, InputNumber, Button } from 'antd';
 import { useIntl, FormattedMessage } from 'umi';
 import * as THREE from 'three'
 import styles from './Welcome.less';
@@ -8,20 +8,43 @@ import STLViewer from 'stl-viewer';
 
 export default (): React.ReactNode => {
     const intl = useIntl();
+    const layout = {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 16 },
+      };
+      const validateMessages = {
+        required: '${label} is required!',
+        types: {
+          email: '${label} is not a valid email!',
+          number: '${label} is not a valid number!',
+        },
+        number: {
+          range: '${label} must be between ${min} and ${max}',
+        },
+      };
+      
     return (
         <PageContainer>
-            <Card>
-                <Alert
-                    message="coming soon"
-                    type="warning"
-                    showIcon
-                    banner
-                    style={{
-                        margin: -12,
-                        marginBottom: 24,
-                    }}
-                />
-            </Card>
+            
+    <Form {...layout} name="nest-messages"  validateMessages={validateMessages}>
+      <Form.Item name={['user', 'name']} label="Name" rules={[{ required: true }]}>
+        <Input />
+      </Form.Item>
+      <Form.Item name={['user', 'email']} label="Email" rules={[{ type: 'email' }]}>
+        <Input />
+      </Form.Item>
+      <Form.Item name={['user', 'age']} label="Age" rules={[{ type: 'number', min: 0, max: 99 }]}>
+        <InputNumber />
+      </Form.Item>
+      <Form.Item name={['user', 'introduction']} label="Introduction">
+        <Input.TextArea />
+      </Form.Item>
+      <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
         </PageContainer>
     );
 };
